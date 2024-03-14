174f3693764aa1948602be96646539d2c8dc9141
Firat Yedibela <yedibelafirat0@gmail.com>
2024-03-14 12:50:47 +0300

    Create simple menu data

---
+++ src/data/menu.js
@@ -0,0 +1,32 @@
+export const soups = [
+  {
+    name: 'Tomato',
+    calories: 83,
+    price: 4.50,
+    image: 'images/meals/tomato.jpg',
+  },
+];
+
+export const mainDishes = [
+  {
+    name: 'Lasagna',
+    calories: 580,
+    price: 12,
+    image: 'images/meals/lasagna.jpg',
+  },
+  {
+    name: 'Pasta Bolognese',
+    calories: 628,
+    price: 10,
+    image: 'images/meals/pasta-bolognese.jpg'
+  },
+]
+
+export const deserts = [
+  {
+    name: 'Chocolate Mousse',
+    calories: 291,
+    price: 7,
+    image: 'images/meals/chocolate-mousse.jpg',
+  },
+];
