ac1e01ab7349afaa65f9dfe7f74e6e925ce8b7e8
Firat Yedibela <yedibelafirat0@gmail.com>
2024-03-15 12:50:05 +0300

    Refactor home-page code

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,21 +1,38 @@
-import RestaurantImage from '../images/restaurant.jpg';
+import '../styles/home-page.css';
+import restaurant from '../images/restaurant.jpg';
+import marker from '../images/map-marker.svg';
 
 export default function renderHomePage() {
   const content = document.querySelector('#content');
-
-  // Create headline
-  const headline = document.createElement('h1');
-  headline.textContent = 'Wonderful Restaurant';
-
-  // Create image
-  const restaurantImage = new Image();
-  restaurantImage.src = RestaurantImage;
-
-  // Create text
+  
+  // Create welcome text
   const infoText = document.createElement('p');
-  infoText.textContent = 'Welcome to our wonderful restaurant. We serve the best food in town since 1976. Enjoy your meal!  '
+  infoText.classList.add('home-info-text');
+  infoText.textContent = 'Welcome to our Wonderful Restaurant. We serve the best food in town since 1976. Enjoy your meal!  '
 
-  content.appendChild(headline);
+  // Create restaurant image
+  const restaurantImage = new Image();
+  restaurantImage.src = restaurant;
+  restaurantImage.classList.add('restaurant-image');
+  
+  // Create address container
+  const address = document.createElement('div');
+  address.classList.add('home-address-container');
+  
+  // Create marker logo
+  const mapMarker = new Image();
+  mapMarker.src = marker;
+  mapMarker.classList.add('home-map-marker');
+
+  // Create address text
+  const addressText = document.createElement('p');
+  addressText.classList.add('home-address');
+  addressText.textContent = 'Odin - 3 Avenue 7/12 Harvard/CS50';
+
+  address.appendChild(mapMarker);
+  address.appendChild(addressText);
+ 
+  content.appendChild(address);
   content.appendChild(restaurantImage);
   content.appendChild(infoText);
 }
