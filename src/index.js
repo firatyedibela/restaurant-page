346f32a485e5c678aa05de6508fb3d5ff79e62c7
Firat Yedibela <yedibelafirat0@gmail.com>
2024-03-15 08:36:48 +0300

    Make nav bar buttons interactive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import renderHomePage from './pages/home.js';
+import renderMenuPage from './pages/menu.js';
 
 renderHomePage();
+
+const navButtons = document.querySelectorAll('.nav-btn');
+navButtons.forEach(navBtn => {
+  navBtn.addEventListener('click', handlePageClick);
+});
+
+function handlePageClick(e) {
+  document.querySelector('#content').textContent = '';
+  switch (e.target.textContent) {
+    case 'Home':
+      renderHomePage();
+      break;
+    case 'Menu':
+      renderMenuPage();
+      break;
+    case 'Contact':
+      renderContactPage();
+      break;
+  }
+}
