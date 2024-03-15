33970c94d4c293e498bdfd6508a2e5cf0b49a306
Firat Yedibela <yedibelafirat0@gmail.com>
2024-03-15 12:34:48 +0300

    Add restaurant logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import renderHomePage from './pages/home-page.js';
 import renderMenuPage from './pages/menu-page.js';
 import renderContactPage from './pages/contact-page.js';
+import restaurantLogo from './images/restaurant-logo.svg';
+import './styles/main.css';
 
 renderHomePage();
 
@@ -23,3 +25,16 @@ function handlePageClick(e) {
       break;
   }
 }
+
+// Create restaurant logo
+const logoContainer = document.querySelector('.restaurant-logo-container');
+const logo = new Image();
+logo.src = restaurantLogo;
+
+logo.classList.add('restaurant-logo');
+
+ // Append restaurant logo
+ if (logoContainer.firstChild) {
+  logoContainer.removeChild(logoContainer.firstChild);
+}
+logoContainer.append(logo);
