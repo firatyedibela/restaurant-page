f274b55333ff757c6b28eec85db80704a81007e8
Firat Yedibela <yedibelafirat0@gmail.com>
2024-03-15 09:31:48 +0300

    Add more products

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,4 +1,22 @@
 import tomato from '../images/meals/tomato.png';
+import broccoli from '../images/meals/broccoli.jpg';
+import beefNoodle from '../images/meals/beef-noodle.jpg';
+import blackBean from '../images/meals/black-bean.jpg';
+import mushroom from '../images/meals/mushroom.jpg';
+import cabbage from '../images/meals/cabbage.jpg';
+import lasagna from '../images/meals/lasagna.jpg';
+import pastaBolognese from '../images/meals/pasta-bolognese.jpg';
+import pizzaMargherita from '../images/meals/pizza-margherita.jpg';
+import pizzaPepperoni from '../images/meals/pizza-pepperoni.jpg';
+import fettucine from '../images/meals/fettucine.jpg';
+import spagetti from '../images/meals/spagetti.jpg';
+import ribEye from '../images/meals/rib-eye.jpg';
+import tunaMaki from '../images/meals/tuna-maki.jpg';
+import sashimi from '../images/meals/sashimi.png';
+import chocolateMousse from '../images/meals/chocolate-mouse.jpg';
+import profiterole from '../images/meals/profiterole.jpg';
+import tiramisu from '../images/meals/tiramisu.jpg';
+import applePie from '../images/meals/apple-pie.jpg';
 
 export const soups = [
   {
@@ -7,6 +25,36 @@ export const soups = [
     price: 4.50,
     image: tomato,
   },
+  {
+    name: 'Broccoli',
+    calories: 104,
+    price: 4,
+    image: broccoli,
+  },
+  {
+    name: 'Beef Noodle Soup',
+    calories: 280,
+    price: 7.50,
+    image: beefNoodle,
+  },
+  {
+    name: 'Black Bean Soup',
+    calories: 258,
+    price: 6.50,
+    image: blackBean,
+  },
+  {
+    name: 'Mushroom Soup',
+    calories: 221,
+    price: 5.50,
+    image: mushroom,
+  },
+  {
+    name: 'Cabbage Soup',
+    calories: 62,
+    price: 3.50,
+    image: cabbage,
+  },
 ];
 
 export const mainDishes = [
@@ -14,13 +62,55 @@ export const mainDishes = [
     name: 'Lasagna',
     calories: 580,
     price: 12,
-    image: 'images/meals/lasagna.jpg',
+    image: lasagna,
   },
   {
     name: 'Pasta Bolognese',
     calories: 628,
     price: 10,
-    image: 'images/meals/pasta-bolognese.jpg'
+    image: pastaBolognese
+  },
+  {
+    name: 'Pizza Margherita',
+    calories: 1317,
+    price: 16,
+    image: pizzaMargherita,
+  },
+  {
+    name: 'Pizza Pepperoni',
+    calories: 298,
+    price: 10,
+    image: pizzaPepperoni
+  },
+  {
+    name: 'Fettuccine',
+    calories: 287,
+    price: 9.50,
+    image: fettucine,
+  },
+  {
+    name: 'Spagetti',
+    calories: 265,
+    price: 9,
+    image: spagetti,
+  },
+  {
+    name: 'Rib Eye Steak',
+    calories: 485,
+    price: 18,
+    image: ribEye,
+  },
+  {
+    name: 'Tuna Maki',
+    calories: 300,
+    price: 14,
+    image: tunaMaki,
+  },
+  {
+    name: 'Sashimi',
+    calories: 160,
+    price: 11,
+    image: sashimi,
   },
 ]
 
@@ -29,6 +119,24 @@ export const deserts = [
     name: 'Chocolate Mousse',
     calories: 291,
     price: 7,
-    image: 'images/meals/chocolate-mousse.jpg',
+    image: chocolateMousse,
+  },
+  {
+    name: 'Profiterole',
+    calories: 477,
+    price: 8,
+    image: profiterole,
+  },
+  {
+    name: 'Tiramisu',
+    calories: 390,
+    price: 6,
+    image: tiramisu,
+  },
+  {
+    name: 'Apple Pie',
+    calories: 361,
+    price: 6,
+    image: applePie,
   },
 ];
